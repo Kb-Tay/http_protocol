57759162e34be193b3f0e745fde341b11a67fe95
Aaron <taykaiboon12@gmail.com>
2026-09-08 23:55:53 +0800

    feat(refactor): use channel to read stream

--- main.go
+++ main.go
@@ -8,53 +8,74 @@ import (
 	"strings"
 )
 
+const BYTES_READ = 8;
+
 func main() {
-	const BYTES_READ = 8;
 	f, err := os.Open("messages.txt")	
 	if err != nil {
 		log.Fatal("Failed to read file")	
 	}
 
-	defer f.Close()
+	ch := getLinesChannel(f)
 	
-	var input = strings.Builder{}
+	for  {
+		line, ok := <- ch 
 
-	for {
-		buf := make([]byte, BYTES_READ)
-		_, err := f.Read(buf)
-	
-		var byteToWrite = buf
-
-		for i, ch := range buf {
-			if ch == '\n' {
-				curr, next := buf[:i], buf[i+1:]
-				input.Write(curr)
-				fmt.Printf("read: %s\n", input.String())
-				input.Reset()
-				byteToWrite = next
-				break
-			}
-		}
-
-		input.Write(byteToWrite)	
-
-		if err == io.EOF {
+		if !ok {
 			break;
 		}
-	}
 
-	if input.Len() > 0 && isEmptyStr(input.String()) {
-		fmt.Printf("read: %s\n", input.String())
+		fmt.Printf("read: %s\n", line)
 	}
 }
 
 func isEmptyStr(buf string) bool {
 	for _, ch := range buf {
 		if ch != '\n' {
-			return false
+			return true 
 		}
 	}
 
-	return true
+	return false 
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer f.Close()
+		var input = strings.Builder{}
+
+		for {
+			buf := make([]byte, BYTES_READ)
+			_, err := f.Read(buf)
+
+			var byteToWrite = buf
+
+			for i, b := range buf {
+				if b == '\n' {
+					curr, next := buf[:i], buf[i+1:]
+					input.Write(curr)
+					ch <- input.String()
+					input.Reset()
+					byteToWrite = next
+					break
+				}
+			}
+
+			input.Write(byteToWrite)	
+
+			if err == io.EOF {
+				break;
+			}
+		}
+		
+		if input.Len() > 0 && !isEmptyStr(input.String()) {
+			ch <- input.String()
+		}
+
+		close(ch)
+	}()
+
+	return ch
 }
 
