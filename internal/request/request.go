8eaff2456977b4783f6c66ca32be74787ad6b8d8
Aaron <taykaiboon12@gmail.com>
2026-09-16 23:04:32 +0800

    bug fix: Update to only read n bytes from buffer to prevent reader x00 values

--- internal/request/request.go
+++ internal/request/request.go
@@ -26,16 +26,17 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	request := Request{} // it will initialised with default values
 	
 	// read chunks within a loop 
-	data := make([]byte, 128)
+	data := make([]byte, 0)
 
 	for {
 		bytes := make([]byte, 8)
 		n, err := reader.Read(bytes)
 		if n > 0 {
-			data = append(data, bytes...)
+			// **Key part: Need to only read the amount you take in
+			// when init a slice of fixed size, the bytes slice will contain 
+			// an array of x00 bytes
+			data = append(data, bytes[:n]...) 		
 		}
-
-		fmt.Println("buffer: " + string(data))
 
 		n, parseErr := request.parse(data)
 
@@ -72,7 +73,7 @@ func (r *Request) parseRequestLine(data []byte) (int, error) {
 
 	method, target, protocol := parts[0], parts[1], parts[2]
 
-	if !isValidMethod(method) {
+  if !isValidMethod(method) {
 		return 0, errors.New("Invalid method")
 	}
 
@@ -81,20 +82,22 @@ func (r *Request) parseRequestLine(data []byte) (int, error) {
 	if err != nil {
 		return 0, err
 	}
+	
+	r.RequestLine.RequestTarget = target
+	r.RequestLine.Method = method
+	r.RequestLine.HttpVersion = version
 
-	r.RequestLine = RequestLine{
-		version, target, method,
-	}
-
-	return len(data), nil
+	return len(requestLine), nil
 }
 
 func (r *Request) parse(data []byte) (int, error) {
 	// reads the byte until the first \r\n 
 	// discards the rest of the Request for now
 	n, err := r.parseRequestLine(data)
+	fmt.Printf("received: %v\n", n)
 
 	if n > 0 {
+		fmt.Printf("Hit: %v", r)
 		r.State = 1
 		return n, nil
 	}
@@ -125,6 +128,7 @@ func parseHttpVersion(protocol string) (string, error) {
 	}
 
 	header, version := parts[0], parts[1]
+	fmt.Printf("header: %s %s", header, version)
 	
 	if header != "HTTP" {
 		return "", errors.New("Invalid protocol")
